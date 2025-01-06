fbf237247b606367c51752bead0274f482f98aa0
Evan Yang <71720197+Evany226@users.noreply.github.com>
2025-01-05 23:29:48 -0500

    Linebreak fix last

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: ['react', 'jest'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['unix', 'windows'],
+    'linebreak-style': 0,
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
